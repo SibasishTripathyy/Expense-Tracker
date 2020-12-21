72335b83c8b788709345471826ea9a0d6638446c
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2020-12-21 16:21:33 +0530

    Added a row which comes into effect when no row is available. Base no expense added row.

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const newAmount = document.getElementById('amount');
 const newRow = document.getElementById('tableBody');
 const addButton = document.getElementById('addNewExpense')
 
+// var tr = document.createElement("tr");
+// tr.setAttribute("id", "deleteThisRow");
+
+// var td = document.createElement("td");
+// td.colSpan = "4";
+// td.innerHTML = "No expenses added yet!";
+
+// newRow.appendChild(tr);
+// tr.appendChild(td);
+
 class addItem {
 	constructor(newNote, newDate, newAmount) {
 		this.createExpense(newNote, newDate, newAmount);
@@ -25,6 +35,7 @@ class addItem {
 
 		td4.onclick = () => {
 			tr.remove();
+			nullRowCheck();
 		}
 
 		newRow.appendChild(tr);
@@ -33,12 +44,27 @@ class addItem {
 		tr.appendChild(td2);
 		tr.appendChild(td3);
 		tr.appendChild(td4);
+		nullRowCheck();
 
 
 	}
 }
 
+nullRowCheck();
+
+function nullRowCheck() {
+	if (newRow.childElementCount == 2) {
+
+		document.getElementById("deleteThisRow").style.display = "table-row";
+
+	} else {
+
+		document.getElementById("deleteThisRow").style.display = "none";
+	}
+}
+
 addButton.addEventListener('click', function() {
+	
 	if(newNote.value != "" && newDate.value != "" && newAmount.value != "") {
 		new addItem(newNote.value, newDate.value, newAmount.value);
 		newNote.value = "";
